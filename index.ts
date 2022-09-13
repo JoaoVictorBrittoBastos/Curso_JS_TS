4bf317b138b60c45f83ea064a8b6a58e88f4d914
João Victor Bastos <jbastos2000@gmail.com>
2022-09-12 22:59:03 -0300

    carregando commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+
+function exercicio1(numero1:number, numero2:number){
+    console.log(`exercicio 1: ${numero1 + numero2}`)
+}
+exercicio1(2, 4)
+
+function exercicio2(n1:number, n2:number, n3:number, n4:number, n5:number){
+    console.log( `exercício 2: ${(n1+n2+n3+n4+n5) / 5}`);
+}
+exercicio2(1, 7, 3, 2, 5);
+
+function exercicio3(kg:number, h:number){
+    console.log( `exercício 3: ${ kg / (h*h) }`);
+}
+exercicio3(71, 1.61);
+
+// 80 kg ÷ (1,80 m × 1,80 m)
